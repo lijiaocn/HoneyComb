71c9f5a9c08ec1efddb1cdd3e67b51bd6afb83fa
lijiao <lijiao@localhost.localdomain>
2015-07-21 19:42:14 +0800

    modify

--- release.sh
+++ release.sh
@@ -168,7 +168,7 @@ func_prepare_release(){
 }
 
 CurPath=`pwd`
-func_yellow_str "`ls ${CurPath}/Config`"
+func_yellow_str "`ls ${CurPath}/Config|grep -v *.sh`"
 echo -n "Choose the Cluster:"
 read Cluster
 
