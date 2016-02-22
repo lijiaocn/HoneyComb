f3a8aac45f251790e6e2ae9aa427f2e6b4eecfa4
lijiao <lijiaocn@foxmail.com>
2016-02-22 17:58:33 +0800

    mv Data into log/

--- Deploy/apiserver.local.secure/8-registry/prepare.sh
+++ Deploy/apiserver.local.secure/8-registry/prepare.sh
@@ -2,8 +2,8 @@
 if [ ! -d certs ];then
 	mkdir certs
 fi
-if [ ! -d Data/registry ];then
-	mkdir -p Data/registry
+if [ ! -d log/Data/registry ];then
+	mkdir -p log/Data/registry
 fi
 cp -rf ../../../AuthnAuthz/registry/output/registry.local  ./certs/
 cp -rf ../../../Compile/registry/out/registry/*  .
