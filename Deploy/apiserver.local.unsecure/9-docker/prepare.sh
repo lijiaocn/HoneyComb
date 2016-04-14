a0360f947f786e5ce9314da2972c3e02d4988049
root <root@ym.gslb.qianxun.com>
2016-04-14 22:05:01 +0800

    复制ca.crt

--- Deploy/apiserver.local.unsecure/9-docker/prepare.sh
+++ Deploy/apiserver.local.unsecure/9-docker/prepare.sh
@@ -5,8 +5,8 @@ if [ ! -d ./log/DockerDat ];then mkdir -p ./log/DockerData; fi
 if [ ! -d certs ];then
 	mkdir certs
 fi
-
+cp ../../../AuthnAuthz/registry/ca/ca.pem   ./certs
 if [ ! -e /etc/docker/certs.d/registry.local:5000/ca.crt ];then
 	sudo mkdir -p  /etc/docker/certs.d/registry.local:5000
-	sudo cp -f ../../../AuthnAuthz/registry/ca/ca.pem  /etc/docker/certs.d/registry.local:5000/ca.crt
+	sudo cp -f ./certs/ca.pem  /etc/docker/certs.d/registry.local:5000/ca.crt
 fi
