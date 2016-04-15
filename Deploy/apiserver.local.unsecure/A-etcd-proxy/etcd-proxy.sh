b5a89d142717233eb684d9e7d52e7c9b133eed71
root <root@ym.gslb.qianxun.com>
2016-04-15 18:51:47 +0800

    重启etcd-proxy时清空历史数据

--- Deploy/apiserver.local.unsecure/A-etcd-proxy/etcd-proxy.sh
+++ Deploy/apiserver.local.unsecure/A-etcd-proxy/etcd-proxy.sh
@@ -2,5 +2,7 @@
 #wget -O library.sh https://raw.githubusercontent.com/lijiaocn/LinuxShell/master/library.sh 2>/dev/null
 . ./config
 . ./library.sh
+if [ -d ./log ];then rm -rf ./log/localnode.etcd;fi
+
 if [ ! -d ./log ];then mkdir ./log; fi
 func_service_template_1 ./bin/etcd ./log  CONFIGS  $1
